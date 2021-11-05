16d243b2b227801fa44c8ef1bf92821f5a3b6a99
shaileh Bisht <shaileshbisht765@gmail.com>
2021-11-06 00:35:47 +0530

    upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import React, { useEffect } from "react";
 import "./App.css";
+import alanBtn from "@alan-ai/alan-sdk-web";
 
 function App() {
+  useEffect(() => {}, []);
+
   return (
     <div className="App">
       <h1>welcome</h1>
